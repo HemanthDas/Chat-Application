85df4ff777e0547834fe9431616dca58059b108f
HemanthDas <hemanthchoudary4@gmail.com>
2023-11-18 12:47:30 +0530

    create new page & components

---
+++ src/components/suggestion.jsx
@@ -0,0 +1,13 @@
+import underconstruction from "../assets/underconstruction.svg";
+const Suggestion = () => {
+  return (
+    <div id="suggestion">
+      <div className="some">
+        <img src={underconstruction} alt="underconstruction" />
+      </div>
+      <label>Under Construction</label>
+    </div>
+  );
+};
+
+export default Suggestion;
